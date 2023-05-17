70a3fe91a8ab284bca5af8acd3155247efdb7b20
lazypool <3416382833@qq.com>
2023-05-17 17:00:10 +0800

    link to mongodb && close after query

---
+++ app/search.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb://mongo:27017";
+const client = new MongoClient(uri);
+
+async function search(filter) {
+	let result;
+	try {
+		await client.connect();
+		console.log('Connected successfully to server');
+
+		const luxund = client.db('luxund');
+		const people = luxund.collection('people');
+
+		console.log(filter)
+		result = await people.find(filter).toArray();
+	} catch(err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
+	return result;
+} 
+
+module.exports = search;
