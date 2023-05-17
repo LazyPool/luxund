19cdb329e7e435343f064a72958cdfd93adea463
lazypool <3416382833@qq.com>
2023-05-17 16:57:26 +0800

    drop before import

--- data/doc/import.sh
+++ data/doc/import.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/bash
-mongoimport --host mongo --db luxund --collection people --type json --file /mongo-seed/人物.json --jsonArray
-mongoimport --host mongo --db luxund --collection animal --type json --file /mongo-seed/动物.json --jsonArray
+mongoimport --host mongo --db luxund --collection people --type json --drop --file /mongo-seed/diary1929.json --jsonArray
