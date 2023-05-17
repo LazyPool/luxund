04fdb119b1513d0da5025c973eabb63c881e03e6
lazypool <3416382833@qq.com>
2023-05-17 16:59:11 +0800

    create /search && implement time research

--- app/index.js
+++ app/index.js
@@ -1,21 +1,22 @@
-// 引入 express 包
 const express = require('express');
+const search = require('/app/search.js');
+const { timeFilter } = require('/app/filters.js')
 
-// 创建 express 应用对象
 const app = express();
 
-// 定义路由处理器
-app.get('/', (req, res) => {
-  // 响应主页请求
-  res.send('Hello World!');
+app.get('/search', async function(req, res) {
+	const { sy, sm, sd, ey, em, ed, ry, rm, rd } = req.query;
+
+	let filter = await timeFilter(sy, sm, sd, ey, em, ed, ry, rm, rd);
+
+	let result = await search(filter);
+	res.send(result);
 });
 
-app.get('/about', (req, res) => {
-  // 响应关于页面请求
-  res.send('This is a simple Express server.');
+ app.use(function(req, res) {
+    res.status(404).send('Not found');
 });
 
-// 让应用对象监听 3000 端口
 app.listen(3000, () => {
   console.log('Server is running on port 3000.');
 });
