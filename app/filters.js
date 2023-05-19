8b4541da3824be5cbd8d72ef8d9cae26715e4cf4
lazypool <3416382833@qq.com>
2023-05-19 13:25:18 +0800

    check date before generate timefilter

--- app/filters.js
+++ app/filters.js
@@ -3,10 +3,16 @@ async function timeFilter(sy, sm, sd, ey, em, ed, ry, rm, rd) {
 	ey = +ey; em = +em; ed = +ed;
 	ry = +ry; rm = +rm; rd = +rd;
 
+	if (!sy || !sm || !sd || !ey || !em || !ed) return {};
+	if (sy < 0 || ey < 0 || sy > ey) return {};
+	if (sy === ey && sm > em) return {};
+	if (sy === ey && sm === em && sd > ed) return {};
+	
+	let timefilter = {};
 	let conditions = [];
 
 	if (sy === ey) {
-		if (sm == em) {
+		if (sm === em) {
 			conditions.push({ 年: sy, 月: sm, 日: { $gte: sd, $lte: ed }});
 		} else {
 			conditions.push({ 年: sy, 月: sm, 日: { $gte: sd }});
@@ -21,7 +27,13 @@ async function timeFilter(sy, sm, sd, ey, em, ed, ry, rm, rd) {
 		conditions.push({ 年: ey, 月: em, 日: { $lte: ed }});
 	}
 
-	return { $or: conditions };
+	timefilter.$or = conditions;
+
+	if (ry) timefilter.年 = ry;
+	if (rm) timefilter.月 = rm;
+	if (rd) timefilter.日 = rd;
+
+	return timefilter;
 }
 
 module.exports = { timeFilter };
