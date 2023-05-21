c52e097beb8f7d0e55015b63ad393837252d2677
lazypool <3416382833@qq.com>
2023-05-21 23:22:19 +0800

    add diary in 1928

--- data/doc/import.sh
+++ data/doc/import.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-mongoimport --host mongo --db luxund --collection people --type json --drop --file /mongo-seed/diary1929.json --jsonArray
+jq -s add /mongo-seed/diary1928.json /mongo-seed/diary1929.json > /mongo-seed/diary.json
+mongoimport --host mongo --db luxund --collection diary --type json --drop --file /mongo-seed/diary.json --jsonArray
