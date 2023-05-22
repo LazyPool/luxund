63f23bffbe73c9ed15d1126fe23dec894060ee8e
lazypool <3416382833@qq.com>
2023-05-22 16:50:35 +0800

    add diary in 1912 1914 1915 1921 and 1925

--- data/doc/import.sh
+++ data/doc/import.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-jq -s add /mongo-seed/diary1928.json /mongo-seed/diary1929.json > /mongo-seed/diary.json
+jq -s add /mongo-seed/diary*.json > /mongo-seed/diary.json
 mongoimport --host mongo --db luxund --collection diary --type json --drop --file /mongo-seed/diary.json --jsonArray
