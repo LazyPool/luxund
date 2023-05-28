8259ed5252ff3f0ed6650e65655197c195bd19dd
lazypool <3416382833@qq.com>
2023-05-28 17:20:26 +0800

    use index to optimize query efficient

--- data/doc/import.sh
+++ data/doc/import.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 jq -s add /mongo-seed/diary*.json > /mongo-seed/diary.json
 mongoimport --host mongo --db luxund --collection diary --type json --drop --file /mongo-seed/diary.json --jsonArray
+mongosh --host mongo --eval 'db.diary.createIndex( { 年: 1, 月: 1, 日: 1 } )' luxund
+mongosh --host mongo --eval 'db.diary.createIndex( { 天气: 1 } )' luxund
+mongosh --host mongo --eval 'db.diary.createIndex( { 事件: 1 } )' luxund
+mongosh --host mongo --eval 'db.diary.createIndex( { 人物: 1 } )' luxund
+mongosh --host mongo --eval 'db.diary.createIndex( { 物件: 1 } )' luxund
+mongosh --host mongo --eval 'db.diary.createIndex( { 地点: 1 } )' luxund
